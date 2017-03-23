68882954359ab4a03b8a404a2364579b37e569a2
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-03-23 18:43:11 -0400

    added filter to produce insurance and patient file

---
+++ filter_ins_pac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+This filter will proccess the file
+and will deliver a file with two cells:
+    [insurance | patient]
+"""
+
+def pipe_in(file):
+    print(file)
+
+
+# def pipe_out(file):
+    #hey
