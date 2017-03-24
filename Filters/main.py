c8a9449e831922b724abea84e8bc5b974c67dc77
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-03-24 13:19:16 -0400

    Created template for filters methods. Main method has been built

--- Filters/main.py
+++ Filters/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 """
 This module will recieve a file, and ...
@@ -6,20 +6,48 @@ This module will recieve a file, and ...
 """
 
 import threading
-import filter_ins_pac
+import filter_ins_pat
+import filter_age_ill
+import filter_ill_time
+import filter_sex_ill
+import filter_ill_inv
+import filter_doc_pat
 
+
+
+
+
+    ####################################################################
+    ###                         pipe in                              ###
+    ####################################################################
 
 def pipe_in(file):
     __main__(file)
 
 
-
+    ####################################################################
+    ###                         pipe out                             ###
+    ####################################################################
 
 def pipe_out(file):
+
     # filter_ins_pac.pipe_in(file)
-    t1 = threading.Thread(target= filter_ins_pac.pipe_in(file))
+    t1 = threading.Thread(target= filter_ins_pat.pipe_in(file))
+    t2 = threading.Thread(target= filter_age_ill.pipe_in(file))
+    t3 = threading.Thread(target= filter_ill_time.pipe_in(file))
+    t4 = threading.Thread(target= filter_sex_ill.pipe_in(file))
+    t5 = threading.Thread(target= filter_ill_inv.pipe_in(file))
+    t6 = threading.Thread(target= filter_doc_pat.pipe_in(file))
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
 
-
+    ####################################################################
+    ###                         "main"                               ###
+    ####################################################################
 
 
 def __main__(file):
